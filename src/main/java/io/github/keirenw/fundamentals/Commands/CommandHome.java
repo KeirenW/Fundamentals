bdb3be276a716ba75f3a3c30ddf005480e1ccd9e
Keiren Waddell <kjwaddell@dundee.ac.uk>
2019-02-26 17:30:00 +0000

    /home working for multiple homes with different names
    TODO:
     + Permissions
     + Travel to others homes.

--- src/main/java/io/github/keirenw/fundamentals/Commands/CommandHome.java
+++ src/main/java/io/github/keirenw/fundamentals/Commands/CommandHome.java
@@ -10,7 +10,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class CommandHome implements CommandExecutor {
+public class CommandHome implements CommandExecutor
+{
 	private Fundamentals plugin;
 	
 	public CommandHome(Fundamentals instance)
@@ -22,26 +23,54 @@ public class CommandHome implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
 		//Checks that the sender of the command is a player and not the console.
-		if(sender instanceof Player)
+		if (sender instanceof Player)
 		{
 			Player player = (Player) sender;
+			World world;
+			double x;
+			double y;
+			double z;
+			double yaw;
+			double pitch;
 			
 			//Check to see if player has a home set and catch exception if not
 			try
 			{
-				//Get the world the home is in
-				String homeWorld = plugin.getConfig().getString("players." + player.getName() + ".home.world");
-				World world = Bukkit.getWorld(homeWorld);
-				//Get X, Y, Z coords for home
-				double xCoord = plugin.getConfig().getDouble("players." + player.getName() + ".home.x");
-				double yCoord = plugin.getConfig().getDouble("players." + player.getName() + ".home.y");
-				double zCoord = plugin.getConfig().getDouble("players." + player.getName() + ".home.z");
-				
-				player.teleport(new Location(world, xCoord, yCoord, zCoord));
+				//Check if a home name is given
+				if (args.length == 0)
+				{
+					//No name given, default to home
+					world = Bukkit.getWorld(plugin.getConfig().getString("player." + player.getName() + ".home.world"));
+					x = plugin.getConfig().getDouble("player." + player.getName() + ".home.x");
+					y = plugin.getConfig().getDouble("player." + player.getName() + ".home.y");
+					z = plugin.getConfig().getDouble("player." + player.getName() + ".home.z");
+					yaw = plugin.getConfig().getDouble("player." + player.getName() + ".home.yaw");
+					pitch = plugin.getConfig().getDouble("player." + player.getName() + ".home.pitch");
+					
+					player.teleport(new Location(world, x, y, z, (float) yaw, (float) pitch));
+				} else if (args.length == 1)
+				{
+					//Name given tp to named home
+					world = Bukkit.getWorld(plugin.getConfig().getString("player." + player.getName() + "." + args[0] + ".world"));
+					x = plugin.getConfig().getDouble("player." + player.getName() + "." + args[0] + ".x");
+					y = plugin.getConfig().getDouble("player." + player.getName() + "." + args[0] + ".y");
+					z = plugin.getConfig().getDouble("player." + player.getName() + "." + args[0] + ".z");
+					yaw = plugin.getConfig().getDouble("player." + player.getName() + "." + args[0] + ".yaw");
+					pitch = plugin.getConfig().getDouble("player." + player.getName() + "." + args[0] + ".pitch");
+					
+					player.teleport(new Location(world, x, y, z, (float) yaw, (float) pitch));
+				} else if (args.length == 2)
+				{
+					//TP to others home
+					//TODO permissions for this and implement.
+					return false;
+				}
 				
 				player.sendMessage(ChatColor.YELLOW + "Teleporting Home...");
-			} catch(Exception e) {
+			} catch (Exception e)
+			{
 				player.sendMessage(ChatColor.RED + "You don't have a home set!");
+				return false;
 			}
 		}
 		return true;
