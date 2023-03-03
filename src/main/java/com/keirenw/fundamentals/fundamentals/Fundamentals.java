2a325427ec104c16599731ea7bfada55f6f632d2
Keiren Waddell <keirenwaddell@gmail.com>
2023-03-03 22:57:54 +0000

    Broke out init processes into methods

--- src/main/java/com/keirenw/fundamentals/fundamentals/Fundamentals.java
+++ src/main/java/com/keirenw/fundamentals/fundamentals/Fundamentals.java
@@ -19,7 +19,26 @@ public final class Fundamentals extends JavaPlugin {
         // Plugin startup logic
         getLogger().info("Starting...");
 
-        //Check for config file, if not found generate from default in jar file
+        CheckForConfigYml();
+
+        RegisterCommands();
+
+        List<NamespacedKey> keys = new ArrayList<>();
+        keys = RegisterRecipes(keys);
+
+        RegisterEventListeners(keys);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("Destroying Fundamentals...");
+    }
+
+    /**
+     * Checks for and then reads or creates the config.yml if it is missing
+     */
+    private void CheckForConfigYml() {
         File file = new File(getDataFolder(), "config.yml");
         if (!file.exists())
         {
@@ -30,8 +49,12 @@ public final class Fundamentals extends JavaPlugin {
             getLogger().info("config.yml found, loading...");
             saveConfig();
         }
+    }
 
-        // Commands
+    /**
+     * Register all the plugins commands
+     */
+    private void RegisterCommands() {
         this.getCommand("ping").setExecutor(new CommandPing());
         this.getCommand("pong").setExecutor(new CommandPong());
         this.getCommand("sethome").setExecutor(new CommandSetHome(this));
@@ -39,21 +62,27 @@ public final class Fundamentals extends JavaPlugin {
         this.getCommand("home").setExecutor(new CommandHome(this));
         this.getCommand("homes").setExecutor(new CommandHomes(this));
         this.getCommand("slap").setExecutor(new CommandSlap(this));
+    }
 
-        // Recipes
-        List<NamespacedKey> keys = new ArrayList<>();
-        // 9 Rotten Flesh -> Leather
+    /**
+     * Register any custom recipes
+     * @param keys NameSpacedKeys tied to each recipe used to register it
+     * @return List\<NameSpacedKeys\> list of registered keys
+     */
+    private List<NamespacedKey> RegisterRecipes(List<NamespacedKey> keys) {
+        // 9 Rotten Flesh --> Leather
         NamespacedKey rf2lKey = new NamespacedKey(this, "leather");
         getServer().addRecipe(new RecipeLeather(getLogger(), rf2lKey).Initialise());
         keys.add(rf2lKey);
 
-        // Event Listeners
-        getServer().getPluginManager().registerEvents(new EventPlayerJoin(getLogger(), keys), this);
+        return keys;
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-        getLogger().info("Destroying Fundamentals...");
+    /**
+     * Register event listeners
+     * @param recipeKeys NameSpacedKeys tied to custom recipes
+     */
+    private void RegisterEventListeners(List<NamespacedKey> recipeKeys) {
+        getServer().getPluginManager().registerEvents(new EventPlayerJoin(getLogger(), recipeKeys), this);
     }
 }
